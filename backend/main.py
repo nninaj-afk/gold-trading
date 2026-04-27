978a034243af059bcb27995645b5a0e01e3ebc1a
Farida Moonrangsee <6710615177@cn103>
2026-04-27 22:02:41 +0700

    port

--- backend/main.py
+++ backend/main.py
@@ -811,4 +811,4 @@ app.add_middleware(
 if __name__ == "__main__":
     import uvicorn
 
-    uvicorn.run(app, host="127.0.0.1", port=8000)
+    uvicorn.run(app, host="127.0.0.1", port=int(os.environ.get("PORT", 10000)))
